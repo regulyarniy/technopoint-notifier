3d1538f9ee7cf20aef25794c7caf21aaf89b7767
@nikolairegulyarniy <nikolairegulyarniy@noorsoft.ru>
2019-07-14 00:29:43 +1000

    add scraper function

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { bot, database, SentryLogger } = require(`./initialize`);
+const getProductPriceById = require(`./getProductPriceById`);
 
 const usersCollection = database.collection(`users`);
 
@@ -17,19 +18,24 @@ bot.hears(/^https:\/\/technopoint.ru\/product\//, async ({ from, message, reply 
     try {
         const productId = message.text.split(`/`)[4];
         const url = message.text.slice(0, message.entities[0].length);
+        const price = await getProductPriceById(productId);
         const user = await usersCollection.doc(from.username);
         const userSnapshot = await user.get();
         if (userSnapshot.exists) {
             const products = userSnapshot.get(`products`);
             const found = products.find(p => p.id === productId);
             if (!found) {
-                products.push();
-                await user.update(products);
+                products.push({ id: productId, url, price });
+                await user.update({ products });
+            } else if (found.price !== price) {
+                const index = products.indexOf(found);
+                products[index].price = price;
+                user.update({ products });
             }
         } else {
-            await user.set({ ...from, products: [{ id: productId, url }] });
+            await user.set({ ...from, products: [{ id: productId, url, price }] });
         }
-        await reply(`Товар сохранен`);
+        await reply(`Товар сохранен. Цена: ${price} руб.`);
     } catch (err) {
         SentryLogger.captureException(err);
     }
